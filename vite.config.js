c44716dfca91e378d4c7e75a68f4120bd051e66a
danibengocheaa <danibengochea@iesebre.com>
2024-11-15 20:13:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,14 @@
-export default {
-// config options
-base: '/'
-}
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/landing-page-mamuts/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'login.html'),
+            },
+        },
+    },
+})
